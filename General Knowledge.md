d4f4b197abe9ef3fdc66e2ed2e52280db2efb531
Skimatoshi <skimatoshi@gmail.com>
2024-06-16 09:15:36 +0800

    Test

---
+++ General Knowledge.md
@@ -0,0 +1,25 @@
+# An object is a container for variables and functions
+
+# Variables | Health , Energy, Stamina, Damage 
+# Functions | Attack, Movement, Animations
+
+Special names only for OOP
+# Variables - > Attributes
+# Functions - > Methods
+
+
+# A class is the blueprint for an object
+
+- Classes precedes objects
+- A class also accepts arguments to customise the object
+
+Why use classes and objects?
+1. They organise complex code
+2. They help to create reusable code
+3. They are used everywhere
+4. Some modules require you to create classes
+5. They make it easier to work with scope
+
+# A dunder method is a method that is not called by the user
+ - Instead is is called by python when something happens
+
---
+++ main.py
@@ -0,0 +1,18 @@
+# class Monster:
+#
+#     def __init__(self,health, energy): #######################################
+#         self.health = health                                                 #
+#         self.energy = energy                                                 #
+#                                                                              #
+                                                                               #                                                                              #
+# monster = Monster(health= 50, energy= 100)   #################################
+
+# print(dir(monster))
+
+
+class Monster:
+
+    def __init__(self,health,energy):
+        self.health = health
+        self.energy = energy
+
